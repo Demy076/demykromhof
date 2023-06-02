f6fbe3ae7bf4a9d9f5068a3321969247246b3160
Demy076 <96407591+Demy076@users.noreply.github.com>
2023-06-02 10:38:38 +0200

    modified:   src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import axios, { AxiosInstance, isAxiosError } from "axios";
+import axios, { AxiosInstance } from "axios";
 import { retrieveRandomFact } from "./app/randomFact/random_fact";
 
 const prepareAxiosInstance = (): AxiosInstance => axios.create();
